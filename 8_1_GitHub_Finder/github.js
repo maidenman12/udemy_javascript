4f781e17dc926a450e6b42cf11ddfeaf7c4eabf7
maidenman12 <richardhirstmaiden@hotmail.co.uk>
2018-12-21 15:50:16 +0000

    8_1 - GitHub Finder
    8_1 - GitHub Finder

---
+++ 8_1_GitHub_Finder/github.js
@@ -0,0 +1,22 @@
+class Github {
+    constructor() {
+        this.client_id = 'ca427cf96de6f4c6092a';
+        this.client_secret = '3428615ffd2f84bba02e6b020b7f85e06637d9b8';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
