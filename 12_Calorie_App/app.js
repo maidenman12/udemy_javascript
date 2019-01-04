fa6cd16bea6578430de1542f3a94742756c91890
maidenman12 <richardhirstmaiden@hotmail.co.uk>
2019-01-04 16:46:49 +0000

    Calorie Project
    Calorie Project

---
+++ 12_Calorie_App/app.js
@@ -0,0 +1,203 @@
+// storage controller
+
+//item controller
+const ItemCtrl = (function() {
+    //item constructor
+    const Item = function(id, name, calories){
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+
+    //data structure/state
+    const data = {
+        items: [
+            // {id: 0, name: 'Steak Dinner', calories: 1200},
+            // {id: 1, name: 'Chicken Dinner', calories: 1000},
+            // {id: 2, name: 'Ice Cream', calories: 500},
+        ],
+        currentItem: null,
+        totalCalories: 0
+    }
+
+    //public methods
+    return {
+        getItems: function() {
+            return data.items;
+        },
+        addItem: function(name, calories) {
+            let ID;
+            // create id
+            if(data.items.length > 0) {
+                ID = data.items[data.items.length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // calories to number
+            calories = parseInt(calories);
+
+            // create new item
+            newItem = new Item(ID, name, calories);
+
+            //add to items array
+            data.items.push(newItem);
+
+            return newItem;
+        },
+        getTotalCalories: function(){
+            let total = 0;
+
+            //loop through items and add cals
+            data.items.forEach(function(item){
+                total += item.calories;
+            });
+            
+            //set total
+            data.totalCalories = total;
+            //return total
+            return data.totalCalories;
+        },
+        logData: function() {
+            return data;
+        }
+    }
+})();
+
+
+
+
+//UI controller
+const UICtrl = (function() {
+    const UISelectors = {
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories',
+        totalCalories: '.total-calories'
+    }
+    
+    //public methods
+    return {
+        populateItemList: function(items) {
+            let html = '';
+
+            items.forEach(function(item) {
+                html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                    <i class="edit-item fa fa-pencil"></i>
+                </a>
+            </li>`;
+            });
+            // insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getItemInput: function(){
+            return {
+                name:document.querySelector(UISelectors.itemNameInput).value,
+                calories:document.querySelector(UISelectors.itemCaloriesInput).value
+            }
+        },
+        addListItem: function(item){
+            //show the list
+            document.querySelector(UISelectors.itemList).style.display = 'block';
+            //create li element
+            const li = document.createElement('li');
+            //add class
+            li.className = 'collection-item';
+            //add id
+            li.id =  `item-${item.id}`;
+            //add html
+            li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+                <i class="edit-item fa fa-pencil"></i>
+            </a>`;
+            //insert item
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
+        },
+        clearInput: function(){
+            document.querySelector(UISelectors.itemNameInput). value = '';
+            document.querySelector(UISelectors.itemCaloriesInput). value = '';
+        },
+        hideList: function(){
+            document.querySelector(UISelectors.itemList).style.display = 'none';
+        },
+        showTotalCalories: function(totalCalories){
+            document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+        },
+        getSelectors: function(){
+            return UISelectors;
+        }
+    }
+
+})();
+
+
+
+
+// app controller
+const App = (function(ItemCtrl, UICtrl) {
+    //load event listeners
+    const loadEventListeners = function(){
+        //get ui selectors
+        const UISelectors = UICtrl.getSelectors();
+
+        //add item event
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSumbit);
+    }
+
+    //add item submit
+    const itemAddSumbit = function(e) {
+        // get form input from UI controller
+        const input = UICtrl.getItemInput();
+
+        //check for name and calories entered
+        if(input.name !== '' && input.calories !== ''){
+            //add item
+            const newItem = ItemCtrl.addItem(input.name, input.calories);
+            //add item to ui list
+            UICtrl.addListItem(newItem);
+            //get total calories
+            const totalCalories = ItemCtrl.getTotalCalories();
+            //add total calories to UI
+            UICtrl.showTotalCalories(totalCalories);
+            //clear fields
+            UICtrl.clearInput();
+        }
+
+        e.preventDefault();
+    }
+
+    //public methods
+    return {
+        init: function(){
+            console.log('Initialising app');
+
+            //fetch items from data structure 
+            const items = ItemCtrl.getItems();
+
+            //check if any items
+            if(items.length === 0){
+                UICtrl.hideList();
+            } else {
+                //populate list with items
+            UICtrl.populateItemList(items);
+            }
+
+             //get total calories
+             const totalCalories = ItemCtrl.getTotalCalories();
+             //add total calories to UI
+             UICtrl.showTotalCalories(totalCalories);
+
+            
+
+            //load event listeners
+            loadEventListeners();
+        }
+    }
+
+})(ItemCtrl, UICtrl);
+
+//initialise
+App.init();
